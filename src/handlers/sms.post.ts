fde8f485b6514db44c2d4b113c64daaee6d4655b
Larry W <l422y@l422y.com>
2023-04-02 22:46:17 -0400

    chore: add editorconfig, reformat code

--- src/handlers/sms.post.ts
+++ src/handlers/sms.post.ts
@@ -9,38 +9,38 @@ const numberMap: { [key: string]: string } = JSON.parse(process.env.NUMBER_MAP |
 const helpCommandResponse = `<Response><Message>Commands:\n? - help\n1-10 - vote</Message></Response>`
 
 export const smsPostHandler = (req: any, server: Server, dataBank: PartytimeData) => {
-    const accountEmail = numberMap[req.body?.To]
-    const cleanBody = req.body?.Body.trim()
-    if (server) {
-        const command = cleanBody.toLowerCase()
-        const numCommand = parseInt(command)
+  const accountEmail = numberMap[req.body?.To]
+  const cleanBody = req.body?.Body.trim()
+  if (server) {
+    const command = cleanBody.toLowerCase()
+    const numCommand = parseInt(command)
 
-        // Set default response
-        let response = "Invalid command... try ? for help"
+    // Set default response
+    let response = "Invalid command... try ? for help"
 
-        switch (true) {
-            // Handle Help
-            case ( command === "?" ):
-                return helpCommandResponse
-            // Handle Numeric Vote
-            case ( Number.isInteger(numCommand) && numCommand > 0 && numCommand < 21 ):
-                response = setVote(accountEmail, server, req.body.From, numCommand, dataBank)
-                break
-            default:
-                break
-        }
-        return `<Response><Message>${response}</Message></Response>`
-    } else {
-        return `<Response><Message>Invalid command... try ? for help</Message></Response>`
+    switch (true) {
+      // Handle Help
+      case ( command === "?" ):
+        return helpCommandResponse
+      // Handle Numeric Vote
+      case ( Number.isInteger(numCommand) && numCommand > 0 && numCommand < 21 ):
+        response = setVote(accountEmail, server, req.body.From, numCommand, dataBank)
+        break
+      default:
+        break
     }
+    return `<Response><Message>${response}</Message></Response>`
+  } else {
+    return `<Response><Message>Invalid command... try ? for help</Message></Response>`
+  }
 }
 
 
 export const setVote = (accountEmail: string, server: Server, from: string, vote: number, databank: PartytimeData) => {
-    if (!databank.currentQueue?.queue[vote - 1]?.id) return `Invalid vote: ${vote}`
-    databank.votes[from] = databank.currentQueue?.queue[vote - 1].id
-    if (server) {
-        server.emit("newVote", {uid: from, body: vote})
-    }
-    return `Vote received: ${databank.currentQueue?.queue[vote - 1].name}`
+  if (!databank.currentQueue?.queue[vote - 1]?.id) return `Invalid vote: ${vote}`
+  databank.votes[from] = databank.currentQueue?.queue[vote - 1].id
+  if (server) {
+    server.emit("newVote", {uid: from, body: vote})
+  }
+  return `Vote received: ${databank.currentQueue?.queue[vote - 1].name}`
 }
--- src/types.ts
+++ src/types.ts
@@ -1,198 +1,114 @@
 export class PartytimeData {
-    currentQueue: ISpotifyQueue | undefined
-    votes: { [key: string]: string } = {}
+  currentQueue: ISpotifyQueue | undefined
+  votes: { [key: string]: string } = {}
 }
 
 export interface ITwilioMessage {
-    ToCountry: string,
-    ToState: string,
-    SmsMessageSid: string,
-    NumMedia: string,
-    ToCity: string,
-    FromZip: string,
-    SmsSid: string,
-    FromState: string,
-    SmsStatus: string,
-    FromCity: string,
-    Body: string,
-    FromCountry: string,
-    To: string,
-    MessagingServiceSid: string,
-    ToZip: string,
-    NumSegments: string,
-    MessageSid: string,
-    AccountSid: string,
-    From: string,
-    ApiVersion: string
+  ToCountry: string,
+  ToState: string,
+  SmsMessageSid: string,
+  NumMedia: string,
+  ToCity: string,
+  FromZip: string,
+  SmsSid: string,
+  FromState: string,
+  SmsStatus: string,
+  FromCity: string,
+  Body: string,
+  FromCountry: string,
+  To: string,
+  MessagingServiceSid: string,
+  ToZip: string,
+  NumSegments: string,
+  MessageSid: string,
+  AccountSid: string,
+  From: string,
+  ApiVersion: string
 }
 
 export interface ISpotifyUser {
-    country: string
-    display_name: string
-    email: string
-    external_urls: {
-        spotify: string
-    }
-    followers: {
-        href: null
-        total: number
-    }
-    href: string
-    id: string
-    images: ISpotifyImage[]
-    product?: string
-    type: string
-    uri: string
+  country: string
+  display_name: string
+  email: string
+  external_urls: {
+    spotify: string
+  }
+  followers: {
+    href: null
+    total: number
+  }
+  href: string
+  id: string
+  images: ISpotifyImage[]
+  product?: string
+  type: string
+  uri: string
 }
 
 export interface ISpotifyImage {
-    height: number
-    url: string
-    width: number
+  height: number
+  url: string
+  width: number
 }
 
 export interface ISpotifyPlaylistOwner extends ISpotifyUser {
-    display_name: string
-    external_urls: {
-        spotify: string
-    }
-    href: string
-    id: string
-    type: string
-    uri: string
+  display_name: string
+  external_urls: {
+    spotify: string
+  }
+  href: string
+  id: string
+  type: string
+  uri: string
 }
 
 export interface ISpotifyPlaylist {
-    collaborative: boolean
-    description: string
-    external_urls: {
-        spotify: string
-    }
+  collaborative: boolean
+  description: string
+  external_urls: {
+    spotify: string
+  }
+  href: string
+  id: string
+  images: ISpotifyImage[]
+  name: string
+  owner: ISpotifyPlaylistOwner
+  primary_color: null
+  public: boolean
+  snapshot_id: string
+  tracks: {
     href: string
-    id: string
-    images: ISpotifyImage[]
-    name: string
-    owner: ISpotifyPlaylistOwner
-    primary_color: null
-    public: boolean
-    snapshot_id: string
-    tracks: {
-        href: string
-        total: number
-    }
-    type: string
-    uri: string
+    total: number
+  }
+  type: string
+  uri: string
 }
 
 export interface ISpotifyArtist {
-    external_urls: {
-        spotify: string
-    }
-    href: string
-    id: string
-    name: string
-    type: string
-    uri: string
+  external_urls: {
+    spotify: string
+  }
+  href: string
+  id: string
+  name: string
+  type: string
+  uri: string
 }
 
 export interface ISpotifyPlaylistTrack {
-    added_at: string
-    added_by: {
-        external_urls: {
-            spotify: string
-        }
-        href: string
-        id: string
-        type: string
-        uri: string
-    }
-    is_local: boolean
-    primary_color: null
-    track: {
-        album: ISpotifyAlbum,
-        artists: ISpotifyArtist[]
-        available_markets: string[]
-        disc_number: number
-        duration_ms: number
-        explicit: boolean
-        external_ids: {
-            isrc: string
-        }
-        external_urls: {
-            spotify: string
-        }
-        href: string
-        id: string
-        is_local: boolean
-        name: string
-        popularity: number
-        preview_url: string
-        track_number: number
-        type: string
-        uri: string
-    }
-    video_thumbnail: {
-        url: null
-    }
-}
-
-export interface ISpotifyUser {
-    country: string
-    display_name: string
-    email: string
+  added_at: string
+  added_by: {
     external_urls: {
-        spotify: string
-    }
-    followers: {
-        href: null
-        total: number
-    }
-    href: string
-    id: string
-    images: ISpotifyImage[]
-    product?: string
-    type: string
-    uri: string
-}
-
-export interface ISpotifyImage {
-    height: number
-    url: string
-    width: number
-}
-
-export interface ISpotifyPlaylistOwner {
-    display_name: string
-    external_urls: {
-        spotify: string
+      spotify: string
     }
     href: string
     id: string
     type: string
     uri: string
-}
-
-export interface ISpotifyPlaylistTrack {
-    added_at: string
-    added_by: {
-        external_urls: {
-            spotify: string
-        }
-        href: string
-        id: string
-        type: string
-        uri: string
-    }
-    is_local: boolean
-    primary_color: null
-    track: ISpotifyTrack
-    video_thumbnail: {
-        url: null
-    }
-}
-
-export interface ISpotifyTrack {
-    id: string
+  }
+  is_local: boolean
+  primary_color: null
+  track: {
     album: ISpotifyAlbum,
     artists: ISpotifyArtist[]
     available_markets: string[]
@@ -200,12 +116,13 @@ export interface ISpotifyTrack {
     duration_ms: number
     explicit: boolean
     external_ids: {
-        isrc: string
+      isrc: string
     }
     external_urls: {
-        spotify: string
+      spotify: string
     }
     href: string
+    id: string
     is_local: boolean
     name: string
     popularity: number
@@ -213,118 +130,201 @@ export interface ISpotifyTrack {
     track_number: number
     type: string
     uri: string
+  }
+  video_thumbnail: {
+    url: null
+  }
+}
+
+export interface ISpotifyUser {
+  country: string
+  display_name: string
+  email: string
+  external_urls: {
+    spotify: string
+  }
+  followers: {
+    href: null
+    total: number
+  }
+  href: string
+  id: string
+  images: ISpotifyImage[]
+  product?: string
+  type: string
+  uri: string
+}
+
+export interface ISpotifyImage {
+  height: number
+  url: string
+  width: number
+}
+
+export interface ISpotifyPlaylistOwner {
+  display_name: string
+  external_urls: {
+    spotify: string
+  }
+  href: string
+  id: string
+  type: string
+  uri: string
+}
+
+export interface ISpotifyPlaylistTrack {
+  added_at: string
+  added_by: {
+    external_urls: {
+      spotify: string
+    }
+    href: string
+    id: string
+    type: string
+    uri: string
+  }
+  is_local: boolean
+  primary_color: null
+  track: ISpotifyTrack
+  video_thumbnail: {
+    url: null
+  }
+}
+
+export interface ISpotifyTrack {
+  id: string
+  album: ISpotifyAlbum,
+  artists: ISpotifyArtist[]
+  available_markets: string[]
+  disc_number: number
+  duration_ms: number
+  explicit: boolean
+  external_ids: {
+    isrc: string
+  }
+  external_urls: {
+    spotify: string
+  }
+  href: string
+  is_local: boolean
+  name: string
+  popularity: number
+  preview_url: string
+  track_number: number
+  type: string
+  uri: string
 }
 
 
 export interface ISpotifyAlbum {
-    "album_type": string,
-    "total_tracks": number,
-    "available_markets": string[],
-    "external_urls": {
-        "spotify": string
-    },
-    "href": string,
-    "id": string,
-    "images": [
-        {
-            "url": string,
-            "height": number,
-            "width": number
-        }]
+  "album_type": string,
+  "total_tracks": number,
+  "available_markets": string[],
+  "external_urls": {
+    "spotify": string
+  },
+  "href": string,
+  "id": string,
+  "images": [
+    {
+      "url": string,
+      "height": number,
+      "width": number
+    }]
 
-    "name": string,
-    "release_date": string,
-    "release_date_precision": string,
-    "restrictions": {
-        "reason": string
-    },
-    "type": string,
-    "uri": string,
-    "copyrights": [
-        {
-            "text": string,
-            "type": string
-        }
-    ],
-    "external_ids": {
-        "isrc": string,
-        "ean": string,
-        "upc": string
-    },
-    "genres": string[],
-    "label": string,
-    "popularity": number,
-    "album_group": string,
-    "artists": ISpotifyArtist[]
+  "name": string,
+  "release_date": string,
+  "release_date_precision": string,
+  "restrictions": {
+    "reason": string
+  },
+  "type": string,
+  "uri": string,
+  "copyrights": [
+    {
+      "text": string,
+      "type": string
+    }
+  ],
+  "external_ids": {
+    "isrc": string,
+    "ean": string,
+    "upc": string
+  },
+  "genres": string[],
+  "label": string,
+  "popularity": number,
+  "album_group": string,
+  "artists": ISpotifyArtist[]
 }
 
 export interface ISpotifyPlayerState {
-    id: string;
-    is_active: boolean,
-    is_private_session: boolean,
-    is_restricted: boolean,
-    name: string,
-    type: string,
-    volume_percent: number
+  id: string;
+  is_active: boolean,
+  is_private_session: boolean,
+  is_restricted: boolean,
+  name: string,
+  type: string,
+  volume_percent: number
 }
 
 export interface ISpotifyPlayerItem {
-    "album": ISpotifyAlbum,
-    "artists": [
-        {
-            "external_urls": {
-                "spotify": string
-            },
-            "followers": {
-                "href": string,
-                "total": 0
-            },
-            "genres": ["Prog rock", "Grunge"],
-            "href": string,
-            "id": string,
-            "images": [
-                {
-                    "url": "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228",
-                    "height": 300,
-                    "width": 300
-                }
-            ],
-            "name": string,
-            "popularity": 0,
-            "type": "artist",
-            "uri": string
-        }
-    ],
-    "available_markets": [string],
-    "disc_number": 0,
-    "duration_ms": 0,
-    "explicit": false,
-    "external_ids": {
-        "isrc": string,
-        "ean": string,
-        "upc": string
-    },
-    "external_urls": {
+  "album": ISpotifyAlbum,
+  "artists": [
+    {
+      "external_urls": {
         "spotify": string
-    },
-    "href": string,
-    "id": string,
-    "is_playable": false,
-    "linked_from": {},
-    "restrictions": {
-        "reason": string
-    },
-    "name": string,
-    "popularity": 0,
-    "preview_url": string,
-    "track_number": 0,
-    "type": "track",
-    "uri": string,
-    "is_local": false
+      },
+      "followers": {
+        "href": string,
+        "total": 0
+      },
+      "genres": ["Prog rock", "Grunge"],
+      "href": string,
+      "id": string,
+      "images": [
+        {
+          "url": "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228",
+          "height": 300,
+          "width": 300
+        }
+      ],
+      "name": string,
+      "popularity": 0,
+      "type": "artist",
+      "uri": string
+    }
+  ],
+  "available_markets": [string],
+  "disc_number": 0,
+  "duration_ms": 0,
+  "explicit": false,
+  "external_ids": {
+    "isrc": string,
+    "ean": string,
+    "upc": string
+  },
+  "external_urls": {
+    "spotify": string
+  },
+  "href": string,
+  "id": string,
+  "is_playable": false,
+  "linked_from": {},
+  "restrictions": {
+    "reason": string
+  },
+  "name": string,
+  "popularity": 0,
+  "preview_url": string,
+  "track_number": 0,
+  "type": "track",
+  "uri": string,
+  "is_local": false
 }
 
 export interface ISpotifyQueue {
-    "currently_playing": ISpotifyTrack,
-    "queue": ISpotifyTrack[]
+  "currently_playing": ISpotifyTrack,
+  "queue": ISpotifyTrack[]
 }
 
